5f7840c62c8f65c31ca5526529221df37cba6265
Lénárt Bence <bence960206@gmail.com>
2019-08-20 22:29:53 +0200

    solution created, nuget packages and pages created

---
+++ MyFavoriteAddresses/Views/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MyFavoriteAddresses.Views
+{
+    [Activity(Label = "MyFavoriteAddresses", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        #region Properties
+
+        private Button _addAddressButton;
+
+        #endregion
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.main_activity);
+
+            _addAddressButton = FindViewById<Button>(Resource.Id.addAddressButton);
+
+            _addAddressButton.Click += AddAddressButton_Click;
+        }
+
+        /// <summary>
+        /// Navigate to the AddAddressActivity.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddAddressButton_Click(object sender, EventArgs e)
+        {
+            StartActivity(new Intent(this, typeof(AddAddressActivity)));
+        }
+    }
+}
